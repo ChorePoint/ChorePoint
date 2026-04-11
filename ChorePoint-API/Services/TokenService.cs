b0c36014ed89a54b889b229c941b6ca64126ea06
TeRaccoon <jack.sam.benning@gmail.com>
2026-04-11 11:16:08 +0100

    Fixed JWT mismatch with validation

--- ChorePoint-API/Services/TokenService.cs
+++ ChorePoint-API/Services/TokenService.cs
@@ -27,8 +27,10 @@ namespace ChorePoint_API.Services
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
                 claims: claims,
-                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_config["Jwt:DurationInMinutes"])),
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_config["Jwt:DurationInMinutes"])),
                 signingCredentials: creds
             );
 
