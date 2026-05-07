3d932291b463b162b92e5f209eddf8242322e0fa
TeRaccoon <jack.sam.benning@gmail.com>
2026-05-07 21:28:48 +0100

    Create chore request

---
+++ ChorePoint-API/Models/Requests/CreateChoreRequest.cs
@@ -0,0 +1,15 @@
+﻿using ChorePoint_API.Enums;
+
+namespace ChorePoint_API.Models.Requests
+{
+    public class CreateChoreRequest
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public int KidId { get; set; }
+        public ChoreFrequency Frequency { get; set; }
+        public DayOfWeek DueDate { get; set; }
+        public int Points { get; set; }
+        public string? Description { get; set; }
+    }
+}
