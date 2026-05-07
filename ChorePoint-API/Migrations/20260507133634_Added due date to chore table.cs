511beb9350b4e85a30b46a54517db42333377694
TeRaccoon <jack.sam.benning@gmail.com>
2026-05-07 21:28:11 +0100

    Added due date for chores table

---
+++ ChorePoint-API/Migrations/20260507133634_Added due date to chore table.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChorePoint_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedduedatetochoretable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "due_date",
+                table: "chores",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "due_date",
+                table: "chores");
+        }
+    }
+}
