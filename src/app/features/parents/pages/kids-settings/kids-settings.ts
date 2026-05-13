fd247f90ab31a009fc8f999644c21bb03c35ea0f
TeRaccoon <jack.sam.benning@gmail.com>
2026-05-13 21:09:55 +0100

    Added functionality to kid settings

--- src/app/features/parents/pages/kids-settings/kids-settings.ts
+++ src/app/features/parents/pages/kids-settings/kids-settings.ts
@@ -1,12 +1,14 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/internal/operators/map';
+import { ChoreService } from '../../../../core/services/chore/chore.service';
 import { UserService } from '../../../../core/services/kids/kids.service';
+import { Chore } from '../../../../core/types/dtos/chore';
 import { LoadingScreen } from '../../../../shared/pages/loading-screen/loading-screen';
-import { Kid } from '../../../kids/models/user';
 import { Leaderboard } from '../../components/leaderboard/leaderboard';
+import { KidDetails } from './types';
 
 @Component({
   selector: 'app-kids-settings',
@@ -15,13 +17,28 @@ import { Leaderboard } from '../../components/leaderboard/leaderboard';
   styleUrl: './kids-settings.scss',
 })
 export class KidsSettings {
+  private choreService = inject(ChoreService);
   private userService = inject(UserService);
 
   vm$!: Observable<{
-    kids: Kid[];
+    kids: KidDetails[];
+    chores: Chore[];
   }>;
 
   ngOnInit() {
-    this.vm$ = this.userService.getKids().pipe(map((kids) => ({ kids })));
+    this.vm$ = combineLatest([this.userService.getKids(), this.choreService.getChores()]).pipe(
+      map(([kids, chores]) => {
+        console.log('kids:', kids);
+        console.log('chores:', chores);
+
+        return {
+          kids: kids.map((kid) => ({
+            ...kid,
+            activeChores: chores.filter((c) => c.userId === kid.id).length,
+          })),
+          chores,
+        };
+      }),
+    );
   }
 }
---
+++ src/app/features/parents/pages/kids-settings/types.ts
@@ -0,0 +1,5 @@
+import { Kid } from '../../../../core/types/dtos/kid';
+
+export type KidDetails = Kid & {
+  activeChores: number;
+};
