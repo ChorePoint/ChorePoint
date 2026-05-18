968e9cfe9cad7ab1fd209ea41512a80807ea459b
TeRaccoon <jack.sam.benning@gmail.com>
2026-05-18 20:45:42 +0100

    Parent settings page

--- src/app/features/parents/pages/parent-settings/parent-settings.ts
+++ src/app/features/parents/pages/parent-settings/parent-settings.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/internal/operators/map';
+import { KidsService } from '../../../../core/services/kids/kids.service';
+import { Kid } from '../../../../core/types/dtos/kid';
+import { LoadingScreen } from '../../../../shared/pages/loading-screen/loading-screen';
+import { KidList } from '../../../chores/components/kid-list/kid-list';
 
 @Component({
   selector: 'app-parent-settings',
-  imports: [],
+  imports: [AsyncPipe, LoadingScreen, KidList],
   templateUrl: './parent-settings.html',
   styleUrl: './parent-settings.scss',
 })
 export class ParentSettings {
+  private userService = inject(KidsService);
 
+  vm$!: Observable<{
+    kids: Kid[];
+  }>;
+
+  ngOnInit() {
+    this.vm$ = this.userService.getKids().pipe(
+      map((kids) => {
+        return { kids };
+      }),
+    );
+  }
 }
