a91cdac52d41aa173f09b15726e4e088db226167
AlxTray <alex.ashby02@icloud.com>
2026-06-15 22:22:21 +0100

    fix: update maximum lengths in input validation to same as database validation

--- ChorePoint.Application/Handlers/Auth/Register/RegisterValidator.cs
+++ ChorePoint.Application/Handlers/Auth/Register/RegisterValidator.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using FluentValidation;
 
 namespace ChorePoint.Application.Handlers.Auth.Register;
@@ -8,20 +9,21 @@ public class RegisterValidator : AbstractValidator<RegisterCommand>
     {
         RuleFor(x => x.FirstName)
             .NotEmpty().WithMessage("First name is required")
-            .MaximumLength(50).WithMessage("First name does not exceed 50 characters")
+            .MaximumLength(100).WithMessage("First name does not exceed 50 characters")
             .Matches(@"^[a-zA-Z\s]+$").WithMessage("First name can only contain letters and spaces");
 
         RuleFor(x => x.LastName)
             .NotEmpty().WithMessage("Last name is required")
-            .MaximumLength(50).WithMessage("Last name does not exceed 50 characters")
+            .MaximumLength(100).WithMessage("Last name does not exceed 50 characters")
             .Matches("^[a-zA-Z]+$").WithMessage("Last name can only contain letters and spaces");
 
         RuleFor(x => x.Email)
             .NotEmpty().WithMessage("Email address is required")
-            .EmailAddress().WithMessage("Invalid email format")
-            .MaximumLength(50).WithMessage("Email does not exceed 100 characters");
+            .MaximumLength(100).WithMessage("Email cannot exceed 100 characters")
+            .EmailAddress().WithMessage("Invalid email format");
 
         RuleFor(x => x.Password)
-            .NotEmpty().WithMessage("Password is required");
+            .NotEmpty().WithMessage("Password is required")
+            .MaximumLength(20).WithMessage("Password cannot exceed 100 characters");
     }
 }
