269439ba75cb92350a00608211df7eeab91c17b1
TeRaccoon <jack.sam.benning@gmail.com>
2026-07-15 20:34:55 +0100

    feature: implemented create kid endpoint and added it to create profile form

--- ChorePoint.Website/src/app/features/start/pages/create-profile/create-profile.ts
+++ ChorePoint.Website/src/app/features/start/pages/create-profile/create-profile.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AVATARS } from '../../../../core/consts/avatars';
+import { KidsService } from '../../../../core/services/kids/kids.service';
+import { LoadingEmoji } from '../../../../shared/components/loading-emoji/loading-emoji';
 
 @Component({
   selector: 'app-create-profile',
   templateUrl: './create-profile.html',
   styleUrl: './create-profile.scss',
+  imports: [LoadingEmoji, ReactiveFormsModule],
 })
 export class CreateProfile {
+  private fb = inject(FormBuilder);
+  private kidService = inject(KidsService);
+  private router = inject(Router);
+
+  loading = signal(false);
+  error = signal<string | null>(null);
+
   avatars = AVATARS;
-  selectedAvatar: string = this.avatars[0].emoji;
+
+  form = this.fb.nonNullable.group({
+    name: ['', { validators: Validators.required }],
+    age: ['', { validators: [Validators.required] }],
+    avatar: ['🧒', { validators: [Validators.required] }],
+  });
 
   selectAvatar(avatar: string) {
-    this.selectedAvatar = avatar;
+    this.form.patchValue({ avatar: avatar });
   }
 
   back() {
@@ -19,6 +36,18 @@ export class CreateProfile {
   }
 
   submit() {
-    console.log('Form submitted');
+    console.log(this.form);
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const { name, age, avatar } = this.form.getRawValue();
+
+    this.kidService.createKid$({ name, age, avatar }).subscribe({
+      next: () => {
+        this.router.navigate(['dashboard/home']);
+      },
+    });
   }
 }
