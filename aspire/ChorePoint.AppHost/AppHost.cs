67be5913158534515cad80e57356499a2dc5d6d1
AlxTray <alex.ashby02@icloud.com>
2026-07-19 19:35:22 +0100

    feature: add database test data seeding

--- aspire/ChorePoint.AppHost/AppHost.cs
+++ aspire/ChorePoint.AppHost/AppHost.cs
@@ -2,26 +2,52 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres").WithPgAdmin().WithDataVolume();
+var seedData = builder.AddParameter("seed-test-data");
+var sensitiveDatabaseLogging = builder.AddParameter("database-log-sensitive-values");
+
+var postgres = builder.AddPostgres("postgres").WithDbGate();
+if (
+    bool.TryParse(
+        await seedData.Resource.GetValueAsync(CancellationToken.None),
+        out var seedDataValue
+    ) && !seedDataValue
+)
+    postgres.WithDataVolume();
+
+var connectionStringAdditions = string.Empty;
+if (
+    bool.TryParse(
+        await sensitiveDatabaseLogging.Resource.GetValueAsync(CancellationToken.None),
+        out var sensitiveDatabaseLoggingValue
+    ) && sensitiveDatabaseLoggingValue
+)
+    connectionStringAdditions = "Include Error Detail=true;Log Parameters=true";
 
 var db = postgres.AddDatabase("chorepoint-db");
+var dbConnection = builder
+    .AddConnectionString(
+        "chorepoint-db-cs",
+        ReferenceExpression.Create($"{db};{connectionStringAdditions}")
+    )
+    .WaitFor(db);
 
 var migrations = builder
     .AddProject<ChorePoint_MigrationService>("migrations")
-    .WithReference(db)
+    .WithEnvironment("SEED_TEST_DATA", seedData)
+    .WithReference(dbConnection)
     .WaitFor(db);
 
 var api = builder
     .AddProject<ChorePoint_API>("api")
     .WithHttpHealthCheck("/health")
-    .WithReference(db)
+    .WithReference(dbConnection)
     .WithReference(migrations)
     .WaitForCompletion(migrations);
 
-var website = builder
+builder
     .AddJavaScriptApp("website", "../../ChorePoint.Website")
     .WithHttpEndpoint(env: "PORT")
     .WithReference(api)
     .WaitFor(api);
 
-builder.Build().Run();
+await builder.Build().RunAsync();
