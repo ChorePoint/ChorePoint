64e9544c327dc2d1d4656997ee573612480795e9
AlxTray <alex.ashby02@icloud.com>
2026-07-22 18:11:10 +0100

    Updated JWT config to use Apire parameters
    and make the JWT key a secret

--- aspire/ChorePoint.AppHost/AppHost.cs
+++ aspire/ChorePoint.AppHost/AppHost.cs
@@ -2,6 +2,11 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var jwtKey = builder.AddParameter("jwt-key", secret: true);
+var jwtIssuer = builder.AddParameter("jwt-issuer");
+var jwtAudience = builder.AddParameter("jwt-audience");
+var jwtDuration = builder.AddParameter("jwt-duration");
+
 var postgres = builder.AddPostgres("postgres").WithPgAdmin().WithDataVolume();
 
 var db = postgres.AddDatabase("chorepoint-db");
@@ -14,6 +19,10 @@ var migrations = builder
 var api = builder
     .AddProject<ChorePoint_API>("api")
     .WithHttpHealthCheck("/health")
+    .WithEnvironment("JWT_KEY", jwtKey)
+    .WithEnvironment("JWT_ISSUER", jwtIssuer)
+    .WithEnvironment("JWT_AUDIENCE", jwtAudience)
+    .WithEnvironment("JWT_DURATION", jwtDuration)
     .WithReference(db)
     .WithReference(migrations)
     .WaitForCompletion(migrations);
