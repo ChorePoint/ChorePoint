648a7e793a2afa09e95b32edddc26a6d8cb4bf97
AlxTray <alex.ashby02@icloud.com>
2026-08-08 21:17:04 +0100

    feat: add migration

---
+++ ChorePoint.Infrastructure/Migrations/20260808200330_AddIsVisibleToKidShopItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChorePoint.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsVisibleToKidShopItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVisible",
+                table: "KidShopItem",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVisible",
+                table: "KidShopItem");
+        }
+    }
+}
