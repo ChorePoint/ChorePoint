ecc1780ce7539086d4297952824a8af53828e434
AlxTray <alex.ashby02@icloud.com>
2026-09-05 20:21:54 +0100

    fix: format issue

--- ChorePoint.Infrastructure/Options/AuthenticationOptions.cs
+++ ChorePoint.Infrastructure/Options/AuthenticationOptions.cs
@@ -4,9 +4,9 @@ public class AuthenticationOptions
 {
     public const string ConfigurationSectionName = "Authentication";
 
-    public string JwtKey { get; set; }
-    public string JwtIssuer { get; set; }
-    public string JwtAudience { get; set; }
+    public string JwtKey { get; set; } = string.Empty;
+    public string JwtIssuer { get; set; } = string.Empty;
+    public string JwtAudience { get; set; } = string.Empty;
     public double JwtDuration { get; set; }
     public double JwtKidDuration { get; set; }
     public int KidLoginCodeLength { get; set; }
