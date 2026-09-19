be3d4873e9a3dfd9a0b801bea6fdf7753c1b874c
TeRaccoon <jack.sam.benning@gmail.com>
2026-09-19 07:56:03 +0100

    fix: page navigation returns to top of scroll position

--- ChorePoint.Website/src/app/features/parents/layout/dashboard-layout/dashboard-layout.ts
+++ ChorePoint.Website/src/app/features/parents/layout/dashboard-layout/dashboard-layout.ts
@@ -1,7 +1,7 @@
-import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { KidsService } from '../../../../core/services/kids/kids.service';
+import { Component, ElementRef, inject } from '@angular/core';
+import {NavigationEnd, Router, RouterModule } from '@angular/router';
 import { DashboardFooterMenu } from '../../../../shared/components/dashboard-footer-menu/dashboard-footer-menu';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-parent-dashboard',
@@ -10,7 +10,19 @@ import { DashboardFooterMenu } from '../../../../shared/components/dashboard-foo
   styleUrl: './dashboard-layout.scss',
 })
 export class DashboardLayout {
-  private kidsService = inject(KidsService);
+  private router = inject(Router);
+  private elementRef = inject(ElementRef);
 
-  kids = this.kidsService.kids;
+  constructor() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const content = this.elementRef.nativeElement.querySelector('.content');
+
+        content?.scrollTo({
+          top: 0,
+          behavior: 'instant'
+        });
+      });
+  }
 }
